89f095b179f02dfac372202fe35af42cf1a61f96
Semmal-Selvan <semmal.business@gmail.com>
2023-01-14 20:01:12 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './jackboys_logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>ASTROWORLD</h1>
+        <p>
+          <i>For this life I cannot change...</i>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
